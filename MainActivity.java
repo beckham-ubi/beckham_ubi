2ecd6f773f4f09eb4da2b2a7bfe6596914b6022e
beckham-ubi <beckham@ubi.cs.ritsumei.ac.jp>
2018-08-04 09:27:10 -0700

    add new file

--- MainActivity.java
+++ MainActivity.java
@@ -10,6 +10,8 @@ import android.os.Handler;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 public class MainActivity extends Activity implements Runnable, SensorEventListener {
     SensorManager sm;
@@ -33,17 +35,26 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
 
     @Override
     public void run() {
-        tv.setText("X-axis : " + gx + "\n"
+        File newfile = new File("c:¥¥tmp¥¥newfile.txt");
+
+
+
+        tv.setText("ファイルの作成に成功しました" + "\n"
+                + "X-axis : " + gx + "\n"
                 + "Y-axis : " + gy + "\n"
                 + "Z-axis : " + gz + "\n"
-                + "合成加速度 : " + gxyz+"\n");
+                + "合成加速度 : " + gxyz + "\n");
         h.postDelayed(this, 500);
+
+
+
+
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sm = (SensorManager) getSystemService(SENSOR_SERVICE);
         List<Sensor> sensors =
                 sm.getSensorList(Sensor.TYPE_ACCELEROMETER);
         if (0 < sensors.size()) {
@@ -69,8 +80,11 @@ public class MainActivity extends Activity implements Runnable, SensorEventListe
         gx = event.values[0];
         gy = event.values[1];
         gz = event.values[2];
-        gxyz =(float) Math.sqrt(gx*gx+gy*gy+gz*gz);
+        gxyz = (float) Math.sqrt(gx * gx + gy * gy + gz * gz);
     }
+
+
+
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
